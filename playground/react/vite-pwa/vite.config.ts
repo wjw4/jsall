6ff479215ed6f70d7ee0fef90b54cba3262b8239
wjw4 <wjw96710@gmail.com>
2025-01-15 23:30:31 +0800

    新增覆寫測試

--- playground/react/vite-pwa/vite.config.ts
+++ playground/react/vite-pwa/vite.config.ts
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      registerType: 'autoUpdate',
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /\.svg$/,
+            handler: 'CacheFirst',
+            method: 'GET',
+            options: {
+              cacheName: 'test',
+              plugins: [
+                {
+                  requestWillFetch: async ({ request }) => {
+                    const url = new URL(request.url)
+                    url.pathname = url.pathname.replace(/^\/\d\//, '/')
+                    return new Request(url, request)
+                  },
+                },
+              ],
+              cacheableResponse: {
+                statuses: [200],
+              }
+            },
+          },
+        ]
+      }
+    }),
+  ],
 })
